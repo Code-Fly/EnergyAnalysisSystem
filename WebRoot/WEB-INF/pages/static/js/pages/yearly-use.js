1a5673084c2111d37edc638db3b2134523fdc5e7
Barrie <barrieshieh@gmail.com>
2015-11-13 08:18:16 +0800

    update yearly page

---
+++ WebRoot/WEB-INF/pages/static/js/pages/yearly-use.js
@@ -0,0 +1,417 @@
+$(document).ready(function() {
+	$("#export-img").click(function() {
+		var chart = $("#chart").getKendoChart();
+		chart.exportImage().done(function(data) {
+			kendo.saveAs({
+				dataURI : data,
+				fileName : "chart.png",
+			});
+		});
+	});
+
+	$("#export-svg").click(function() {
+		var chart = $("#chart").getKendoChart();
+		chart.exportSVG().done(function(data) {
+			kendo.saveAs({
+				dataURI : data,
+				fileName : "chart.svg",
+			});
+		});
+	});
+
+	$("#export-excel").click(function(e) {
+		var grid = $("#grid").data("kendoGrid");
+		grid.saveAsExcel();
+	});
+
+	$("#reset-dp").click(function() {
+		var user1 = $("#user-1").data("kendoComboBox");
+		var user2 = $("#user-2").data("kendoComboBox");
+		var user3 = $("#user-3").data("kendoComboBox");
+		end.max(new Date(today.getFullYear(), today.getMonth() + 1, 0));
+		end.value(today);
+		start.max(end.value());
+		start.value(today);
+		user1.text(null);
+		user1.value(null);
+		user2.text(null);
+		user2.value(null);
+		user3.text(null);
+		user3.value(null);
+	});
+
+	$("#tab-chart").on('shown.bs.tab', function(e) {
+		reloadChart();
+	})
+
+	$("#tab-data").on('shown.bs.tab', function(e) {
+		reloadGrid();
+	})
+
+	$("#submit-dp").click(function() {
+		reloadChart();
+		reloadGrid();
+	});
+
+	var today = new Date();
+
+	var start = $("#start").kendoDatePicker({
+		depth : "decade",
+		start : "decade",
+		value : today,
+		format : "yyyy",
+		culture : "zh-CN"
+	}).data("kendoDatePicker");
+
+	$("#user-1").kendoComboBox({
+		placeholder : "请选择",
+		dataTextField : "nm",
+		dataValueField : "infoID",
+		dataSource : {
+			transport : {
+				read : {
+					url : _ctx + "/api/tree/query",
+					dataType : "jsonp"
+				}
+			}
+		},
+		change : function(e) {
+			var user1 = $("#user-1").data("kendoComboBox");
+			var user2 = $("#user-2").data("kendoComboBox");
+			user2.setDataSource({
+				transport : {
+					read : {
+						url : _ctx + "/api/tree/query?infoID=" + user1.value(),
+						dataType : "jsonp"
+					}
+				}
+			});
+			user2.text(null);
+			user2.value(null);
+		}
+	});
+
+	$("#user-2").kendoComboBox({
+		placeholder : "请选择",
+		dataTextField : "nm",
+		dataValueField : "infoID",
+		dataSource : null
+	});
+
+	$("#chart").kendoChart({
+		dataSource : {
+			transport : {
+				read : {
+					url : _ctx + "/api/yearaccount/query?infoID=" + $("#user-2").data("kendoComboBox").value() + "&year=" + $("#start").val(),
+					dataType : "jsonp"
+				}
+			}
+		},
+		series : [ {
+			field : "janNumber",
+			name : "一月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+			color : "#ff1c1c",
+		// axis : "janNumber"
+		}, {
+			field : "febNumber",
+			name : "二月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+			color : "#ffae00",
+		// axis : "febNumber"
+		}, {
+			field : "marNumber",
+			name : "三月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+			color : "#73c100",
+		// axis : "marNumber"
+		}, {
+			field : "aprNumber",
+			name : "四月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+			color : "#007eff",
+		// axis : "aprNumber"
+		}, {
+			field : "mayNumber",
+			name : "五月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+			color : "#f3ac32",
+		// axis : "mayNumber"
+		}, {
+			field : "junNumber",
+			name : "六月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+			color : "#b8b8b8",
+		// axis : "junNumber"
+		}, {
+			field : "julNumber",
+			name : "七月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+			color : "#bb6e36",
+		// axis : "julNumber"
+		}, {
+			field : "augNumber",
+			name : "八月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+		// color : "#007eff",
+		// axis : "augNumber"
+		}, {
+			field : "sepNumber",
+			name : "九月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+		// color : "#007eff",
+		// axis : "sepNumber"
+		}, {
+			field : "octNumber",
+			name : "十月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+		// color : "#007eff",
+		// axis : "octNumber"
+		}, {
+			field : "novNumber",
+			name : "十一月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+		// color : "#007eff",
+		// axis : "novNumber"
+		}, {
+			field : "decNumber",
+			name : "十二月 [kwh]",
+			type : "column",
+			markers : {
+				visible : false
+			},
+		// color : "#007eff",
+		// axis : "decNumber"
+		} ],
+		// valueAxes : [ {
+		// name : "janNumber",
+		// color : "#007eff"
+		// }, {
+		// name : "febNumber",
+		// color : "#007eff"
+		// }, {
+		// name : "marNumber",
+		// color : "#007eff"
+		// }, {
+		// name : "aprNumber",
+		// color : "#007eff"
+		// }, {
+		// name : "mayNumber",
+		// color : "#007eff"
+		// }, {
+		// name : "junNumber",
+		// color : "#007eff"
+		// }, {
+		// name : "julNumber",
+		// color : "#007eff"
+		// }, {
+		// name : "augNumber",
+		// color : "#007eff"
+		// }, {
+		// name : "sepNumber",
+		// color : "#007eff"
+		// }, {
+		// name : "octNumber",
+		// color : "#007eff"
+		// }, {
+		// name : "novNumber",
+		// color : "#007eff"
+		// }, {
+		// name : "decNumber",
+		// color : "#007eff"
+		// }],
+		legend : {
+			position : "bottom"
+		},
+		title : {
+			text : $("#start").val()
+		},
+		categoryAxis : {
+			majorGridLines : {
+				visible : false
+			},
+			majorTicks : {
+				visible : false
+			}
+		// axisCrossingValues : [ 999999 ],
+		},
+		tooltip : {
+			visible : true,
+			template : "<div>#= series.name #: #= value #</div>"
+		},
+		chartArea : {
+			background : "transparent"
+		},
+		dataBound : function(e) {
+			// alert($("#user-2").data("kendoComboBox").value())
+		}
+	});
+
+	$("#grid").kendoGrid({
+		excel : {
+			fileName : "Export.xlsx",
+			filterable : true,
+			allPages : true
+		},
+		dataSource : {
+			transport : {
+				read : {
+					url : _ctx + "/api/yearaccount/query?infoID=" + $("#user-2").data("kendoComboBox").value() + "&year=" + $("#start").val(),
+					dataType : "jsonp"
+				}
+			},
+			pageSize : 10,
+		},
+		sortable : true,
+		filterable : true,
+		pageable : {
+			refresh : true,
+			pageSizes : true,
+			buttonCount : 5
+		},
+		selectable : "row",
+		change : function(e) {
+			var selectedRows = this.select();
+			var selectedDataItems = [];
+			for (var i = 0; i < selectedRows.length; i++) {
+				var dataItem = this.dataItem(selectedRows[i]);
+				selectedDataItems.push(dataItem);
+			}
+			// selectedDataItems contains all selected data items
+			// alert(JSON.stringify(selectedDataItems));
+			this.dataSource.read();
+		},
+		dataBound : function(e) {
+			var data = this.dataSource.data();
+			$.each(data, function(i, row) {
+				if (row.stopFlag == 1) {
+					$('tr[data-uid="' + row.uid + '"] ').css("color", "red");
+				}
+			});
+		},
+		columns : [ {
+			locked : true,
+			field : "ID",
+			title : "ID",
+			width : 200
+		}, {
+			field : "collectYear",
+			title : "采集时间",
+			width : 200
+		}, {
+			field : "janNumber",
+			title : "一月",
+			width : 200
+		}, {
+			field : "febNumber",
+			title : "二月",
+			width : 200
+		}, {
+			field : "marNumber",
+			title : "三月",
+			width : 200
+		}, {
+			field : "aprNumber",
+			title : "四月",
+			width : 200
+		}, {
+			field : "mayNumber",
+			title : "五月",
+			width : 200
+		}, {
+			field : "junNumber",
+			title : "六月",
+			width : 200
+		}, {
+			field : "julNumber",
+			title : "七月",
+			width : 200
+		}, {
+			field : "augNumber",
+			title : "八月",
+			width : 200
+		}, {
+			field : "sepNumber",
+			title : "九月",
+			width : 200
+		}, {
+			field : "octNumber",
+			title : "十月",
+			width : 200
+		}, {
+			field : "novNumber",
+			title : "十一月",
+			width : 200
+		}, {
+			field : "decNumber",
+			title : "十二月",
+			width : 200
+		} ],
+
+	});
+
+	function reloadChart() {
+		var chart = $("#chart").data("kendoChart");
+		chart.setOptions({
+			dataSource : {
+				transport : {
+					read : {
+						url : _ctx + "/api/yearaccount/query?infoID=" + $("#user-2").data("kendoComboBox").value() + "&year=" + $("#start").val(),
+						dataType : "jsonp"
+					}
+				}
+			},
+			title : {
+				text : $("#start").val()
+			},
+		});
+	}
+
+	function reloadGrid() {
+		var grid = $("#grid").data("kendoGrid");
+		grid.setOptions({
+			dataSource : {
+				transport : {
+					read : {
+						url : _ctx + "/api/yearaccount/query?infoID=" + $("#user-2").data("kendoComboBox").value() + "&year=" + $("#start").val(),
+						dataType : "jsonp"
+					}
+				},
+				pageSize : 20,
+			}
+		});
+	}
+});
--- WebRoot/WEB-INF/pages/yearly-use.jsp
+++ WebRoot/WEB-INF/pages/yearly-use.jsp
@@ -1,28 +1,86 @@
 <%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
-<%
-String path = request.getContextPath();
-String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
-%>
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<%@ include file="meta/config.jsp"%>
+<!DOCTYPE html>
 <html>
-  <head>
-    <base href="<%=basePath%>">
-    
-    <title>My JSP 'yearly-use.jsp' starting page</title>
-    
-	<meta http-equiv="pragma" content="no-cache">
-	<meta http-equiv="cache-control" content="no-cache">
-	<meta http-equiv="expires" content="0">    
-	<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
-	<meta http-equiv="description" content="This is my page">
-	<!--
-	<link rel="stylesheet" type="text/css" href="styles.css">
-	-->
+<head>
+<title>能源监测系统</title>
+<%@ include file="meta/meta.jsp"%>
+<script type="text/javascript" src="${ctx}/static/js/pages/yearly-use.js"></script>
+</head>
+<body>
+	<div id="wrapper">
+		<%@ include file="meta/navigator.jsp"%>
+		<!-- /#Navigation -->
+		<div id="page-wrapper">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1 class="page-header">年用量分析</h1>
+						<div class="row show-grid">
+							<div class="col-md-4">
+								<input id="user-1" style="width: 100%;" />
+							</div>
+							<div class="col-md-4">
+								<input id="user-2" style="width: 100%;" />
+							</div>
+							<div class="col-md-4">
+								<input id="start" style="width: 100%;" />
+							</div>
+						</div>
+						<div class="row show-grid">
+							<div class="col-md-12 text-center">
+								<button id="submit-dp" class='k-button'>提交</button>
+								<button id="reset-dp" class='k-button'>重置</button>
+							</div>
+						</div>
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<i class="fa fa-bar-chart-o fa-fw"></i> 信息
+								<div class="pull-right">
+									<div class="btn-group">
+										<button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+											导出 <span class="caret"></span>
+										</button>
+										<ul class="dropdown-menu pull-right" role="menu">
+											<li><a href="#" id="export-img">导出PNG图</a></li>
+											<li><a href="#" id="export-svg">导出SVG图</a></li>
+											<li class="divider"></li>
+											<li><a href="#" id="export-excel">导出Excel</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- /.panel-heading -->
+							<div class="panel-body">
+								<!-- Nav tabs -->
+								<ul class="nav nav-tabs">
+									<li class="active"><a id="tab-chart" href="#tab-content-chart" data-toggle="tab">柱状图</a></li>
+									<li><a id="tab-data" href="#tab-content-data" data-toggle="tab">数据表</a></li>
+								</ul>
 
-  </head>
-  
-  <body>
-    This is my JSP page. <br>
-  </body>
+								<!-- Tab panes -->
+								<div class="tab-content">
+									<div class="tab-pane fade in active" id="tab-content-chart">
+										<div id="chart" style="position: relative; background: url(${ctx}/static/images/world-map.png) 50% 50% no-repeat;"></div>
+									</div>
+									<div class="tab-pane fade" id="tab-content-data">
+										<div id="grid" style="margin-top: 1em;"></div>
+									</div>
+
+								</div>
+							</div>
+							<!-- /.panel-body -->
+						</div>
+						<!-- /.panel -->
+					</div>
+					<!-- /.col-lg-12 -->
+				</div>
+				<!-- /.row -->
+			</div>
+			<!-- /.container-fluid -->
+		</div>
+		<!-- /#page-wrapper -->
+	</div>
+	<!-- /#wrapper -->
+</body>
 </html>
