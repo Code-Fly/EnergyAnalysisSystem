c41e20dc42663092487f8e7357003124ca5cf1d4
VM <VM@vm-c0a5ffebea04>
2015-11-13 14:59:28 +0800

    systemError

--- src/com/flycode/common/BaseController.java
+++ src/com/flycode/common/BaseController.java
@@ -7,6 +7,10 @@ package com.flycode.common;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 
@@ -17,4 +21,16 @@ import org.slf4j.LoggerFactory;
 public abstract class BaseController {
 	protected Logger logger = LoggerFactory.getLogger(getClass());
 	
+	/**
+	 * 运行时异常的处理方法
+	 * @param ex
+	 * @return
+	 */
+	@ExceptionHandler(RuntimeException.class)
+	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+	@ResponseBody
+	public String handleUnexpectedServerError(RuntimeException ex) {
+		logger.error("内部错误", ex);
+		return "System Error.";
+	}
 }
